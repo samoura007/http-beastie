cbecc49da151d2c93f35f783a96f27996b5ef8a2
Samoura007 <samer007@lavache.com>
2026-05-03 10:18:15 +0300

    did utils .cpp part

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,5 +1,6 @@
 #pragma once
-
+#include <string>
 #include <boost/beast/http.hpp>
+std::string utility(const std::string& path);
 
 boost::beast::http::response<boost::beast::http::string_body> handle_request(const boost::beast::http::request<boost::beast::http::string_body>& req);
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,4 +1,6 @@
 #include "utils.hpp"
+#include <fstream>
+#include <sstream>
 
 namespace beast = boost::beast;
 namespace http = beast::http;
@@ -28,4 +30,13 @@ http::response<http::string_body> handle_request(const http::request<http::strin
 
   res.prepare_payload();
   return res;
+}
+std::string utility(const std::string& path){
+    std::ifstream file(path);
+    if(!file.is_open()){
+        return "";
+    }
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
 }
